78c5a3d1120f4f34050d2e1f031bb81fcb5c95e3
Enea <enea@prooffactor.com>
2020-11-13 09:29:38 -0500

    Use create_lock_manager

--- lib/activejob/locking/adapters/pg_lock.rb
+++ lib/activejob/locking/adapters/pg_lock.rb
@@ -4,16 +4,16 @@ module ActiveJob
   module Locking
     module Adapters
       class PgLock < Base
-        def lock_manager
+        def create_lock_manager
           ::PgLock.new(ttl: self.options.lock_time, name: self.key, attempts: 3, attempt_interval: 5)
         end
 
         def lock
-          self.lock_manager.create
+          self.create_lock_manager.create
         end
 
         def unlock
-          self.lock_manager.delete
+          self.create_lock_manager.delete
         end
       end
     end
